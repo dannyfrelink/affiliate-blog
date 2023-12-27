e4861c72357bfac0111f626835bc96e44c9f7863
dannyfrelink <danny-frelink@hotmail.com>
2023-12-27 18:53:35 +0100

    feat: create HeaderLarge component with different styling options

--- src/components/header/HeaderLarge.tsx
+++ src/components/header/HeaderLarge.tsx
@@ -1,10 +1,45 @@
-interface HeaderLargeProps {}
+import React from "react";
 
-const HeaderLarge = () => {
+interface HeaderLargeProps {
+	align?: "center" | "bottom";
+	Image: React.FC<JSX.IntrinsicElements["img"]>;
+	title: string;
+	subTitle: string;
+}
+
+const HeaderLarge: React.FC<HeaderLargeProps> = ({
+	align = "center",
+	Image,
+	title,
+	subTitle,
+}) => {
 	return (
-		<div>
-			<div></div>
-		</div>
+		<header
+			className={`${
+				align === "center" && ""
+			} h-screen [&>img]:h-full [&>img]:w-full [&>img]:object-cover [&>img]:object-center [&>img]:absolute [&>img]:z-[-1]`}
+		>
+			<Image />
+
+			<section
+				className={`${
+					align === "center"
+						? "justify-center items-center text-center"
+						: "justify-end pb-5"
+				} flex flex-col h-[90vh] w-5/6 mx-auto text-white`}
+			>
+				<h1 className="font-extrabold text-2xl mb-3">{title}</h1>
+				<p
+					className={`${
+						align === "center"
+							? "font-semibold w-3/4 mx-auto"
+							: "text-[#C3C3C3]"
+					} text-sm`}
+				>
+					{subTitle}
+				</p>
+			</section>
+		</header>
 	);
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import HeaderLarge from "../components/header/HeaderLarge";
+import HeaderImage from "../images/mockup/dreamy.png";
 
 function Home() {
-  return (
-    <div>
-      <h1>Hello World Home</h1>
-    </div>
-  );
+	return (
+		<div>
+			<HeaderLarge
+				Image={() => <img src={HeaderImage} alt="Viewpoint" />}
+				title="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+				subTitle="Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
+			/>
+
+			{/* <HeaderLarge
+				align="bottom"
+				Image={() => <img src={HeaderImage} alt="Viewpoint" />}
+				title="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+				subTitle="28 augustus, 2023"
+			/> */}
+		</div>
+	);
 }
 
 export default Home;
