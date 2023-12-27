3cf70c37312a69738d29ef5379f6b1be1546a913
dannyfrelink <danny-frelink@hotmail.com>
2023-12-27 17:30:39 +0100

    update: format with Prettier

--- src/config/AppContext.tsx
+++ src/config/AppContext.tsx
@@ -1,36 +1,40 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
 
 interface AppContextProps {
-  // Define your context state and any functions you need
-  count: number;
-  increment: () => void;
+	// Define your context state and any functions you need
+	count: number;
+	increment: () => void;
 }
 
 const AppContext = createContext<AppContextProps | undefined>(undefined);
 
 interface AppProps {
-  children: JSX.Element;
+	children: JSX.Element;
 }
 
 export const AppProvider: React.FC<AppProps> = ({ children }) => {
-  const [count, setCount] = useState<number>(0);
+	const [count, setCount] = useState<number>(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+	const increment = () => {
+		setCount(count + 1);
+	};
 
-  const contextValue: AppContextProps = {
-    count,
-    increment,
-  };
+	const contextValue: AppContextProps = {
+		count,
+		increment,
+	};
 
-  return <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>;
+	return (
+		<AppContext.Provider value={contextValue}>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export const useAppContext = () => {
-  const context = useContext(AppContext);
-  if (!context) {
-    throw new Error('useAppContext must be used within an AppProvider');
-  }
-  return context;
-}
+	const context = useContext(AppContext);
+	if (!context) {
+		throw new Error("useAppContext must be used within an AppProvider");
+	}
+	return context;
+};
--- src/config/AppRouter.tsx
+++ src/config/AppRouter.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from '../pages/About';
-import Home from '../pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home";
 
 const AppRouter: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/over-ons" element={<About />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/over-ons" element={<About />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default AppRouter;
