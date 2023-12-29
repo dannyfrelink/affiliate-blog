82970cca40f997d4f3c09be7c6dd676d00275d8e
dannyfrelink <danny-frelink@hotmail.com>
2023-12-29 14:31:35 +0100

    feat: add custom color classes for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#FFF8F2",
+				secondary: "868198",
+				tertair: "687B68",
+				background: "E8DFD1",
+			},
+		},
 	},
 	plugins: [],
 };
