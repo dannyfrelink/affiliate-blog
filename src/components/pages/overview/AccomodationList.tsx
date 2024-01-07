4da26ef999872a5e5865a821e031035548610fa6
dannyfrelink <danny-frelink@hotmail.com>
2024-01-07 12:31:20 +0100

    update: make AccommodationList component responsive

--- src/components/pages/overview/AccomodationList.tsx
+++ src/components/pages/overview/AccomodationList.tsx
@@ -20,36 +20,82 @@ const AccommodationList: React.FC<AccommodationListProps> = ({
 	const { screenSize } = useAppContext();
 
 	return (
-		<div className="[&>*:not(:last-child)]:mb-10">
+		<div className="[&>div:first-child>div]:!pt-0">
 			{destinations.map((dest, index) => {
 				const accoms = accommodations[dest];
 				const destId = dest.toLowerCase().split(" ").join("-");
 
 				return (
 					<div key={index}>
-						<Element className="text-center" name={destId}>
+						<Element
+							className={`text-center ${
+								screenSize < 900
+									? "pt-10"
+									: screenSize < 1250
+									? "pt-16 pb-8"
+									: "pt-20 pb-10"
+							}`}
+							name={destId}
+						>
 							<BaseText>Leukste accommodaties in</BaseText>
-							<H2 className="text-tertair mt-0.5" id={destId}>
+							<H2
+								className={`text-tertair ${
+									screenSize < 900
+										? "mt-0.5"
+										: screenSize < 1250
+										? "mt-1.5"
+										: "mt-2"
+								}`}
+								id={destId}
+							>
 								{dest}
 							</H2>
 						</Element>
 
-						<section className="[&>*:not(:last-child)]:pb-5">
+						<section
+							className={`flex  ${
+								screenSize < 900
+									? "items-center flex-col [&>*:not(:last-child)]:pb-5"
+									: `justify-between flex-wrap ${
+											screenSize < 1250
+												? "gap-y-14"
+												: "gap-y-20"
+									  }`
+							}`}
+						>
 							{accoms.map((accom, index) => {
 								const image = require(`../../../images/mockup/${accom.image}`);
 
 								return (
 									<div
-										className="[&>*:not(:first-child)]:mt-2 pt-5"
+										className={
+											screenSize < 900
+												? "[&>*:not(:first-child)]:mt-2 pt-5 max-w-[550px]"
+												: `w-[35vw] ${
+														screenSize < 1250
+															? "[&>*:not(:first-child)]:mt-2.5"
+															: "[&>*:not(:first-child)]:mt-3"
+												  }`
+										}
 										key={index}
 									>
 										<img
 											src={image}
 											alt="Accommodatie"
-											className="mb-4 rounded-2xl shadow-subtle"
+											className="mb-4 w-full max-h-[325px] object-cover object-center rounded-2xl shadow-subtle"
 										/>
 
-										<H3>{accom.name}</H3>
+										<H3
+											className={
+												screenSize < 900
+													? "!mt-3"
+													: screenSize < 1250
+													? "!mt-4"
+													: "!mt-6"
+											}
+										>
+											{accom.name}
+										</H3>
 										<BaseText>{accom.description}</BaseText>
 
 										<div>
@@ -74,10 +120,16 @@ const AccommodationList: React.FC<AccommodationListProps> = ({
 
 										<Button
 											link={accom.link}
-											className="!mt-3"
+											className={
+												screenSize < 900
+													? "!mt-3"
+													: screenSize < 1250
+													? "!mt-4"
+													: "!mt-5"
+											}
 											blank
 										>
-											Meer informatie{" "}
+											Bekijk accommodatie{" "}
 											<ArrowForwardRoundedIcon
 												fontSize={
 													screenSize < 750
