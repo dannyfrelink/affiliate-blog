8ac0a3a7af899dac70c635dd7e79611107ae40bb
dannyfrelink <danny-frelink@hotmail.com>
2024-01-07 14:41:03 +0100

    update: implement new IntroOverview component

--- src/pages/Accommodations.tsx
+++ src/pages/Accommodations.tsx
@@ -9,6 +9,7 @@ import Footer from "../components/general/Footer";
 import data from "../data/accommodations.json";
 import AccommodationList from "../components/pages/overview/AccomodationList";
 import ScrollToTopButton from "../components/pages/overview/ScrollToTopButton";
+import IntroOverview from "../components/pages/overview/IntroOverview";
 
 export interface Destination {
 	image: string;
@@ -45,57 +46,36 @@ const Accommodations = () => {
 			/>
 
 			<Overview destinations={destinations}>
-				<div className="max-w-[1000px] mx-auto">
-					<H2 className={screenSize < 1250 ? "mb-2" : "mb-3"}>
-						Lorem ipsum dolor sit amet est explicabo blanditiis
-					</H2>
-					<div
-						className={
-							screenSize < 1250
-								? "[&>*:not(:last-child)]:mb-3"
-								: "[&>*:not(:last-child)]:mb-4"
-						}
-					>
-						<BaseText>
-							Lorem ipsum dolor sit amet. Est explicabo blanditiis
-							eum perferendis harum eum galisum voluptas quo natus
-							nihil aut aspernatur voluptas rem ipsum dolorum aut
-							fugiat cumque. Et officiis iure et asperiores totam
-							quo sunt alias. Sed illum perferendis quo possimus
-							inventore et reiciendis modi. Sed quos voluptate nam
-							deleniti veniam ut nihil consequatur.
-						</BaseText>
-						<div
-							className={
-								screenSize < 1250
-									? "[&>*:not(:last-child)]:mb-3"
-									: "[&>*:not(:last-child)]:mb-4"
-							}
-						>
-							<BaseText>
-								Et dolorem rerum qui doloremque consectetur aut
-								incidunt fugit ad voluptatibus dignissimos aut
-								laborum excepturi et dicta dicta quo fuga enim.
-								Est ipsum possimus a corporis dolores qui
-								placeat dolor aut veritatis eveniet non cumque
-								autem sed iure veritatis est suscipit sequi. Est
-								asperiores porro non officia incidunt sed autem
-								dolor est dolores illo et molestiae quas aut
-								error totam.
-							</BaseText>
-							<BaseText>
-								Et optio veniam aut veniam voluptatem et
-								explicabo ullam id aperiam cumque aut neque
-								laborum ex voluptatem facere. Eum rerum aperiam
-								eos fugiat dolor et quis cupiditate sed animi
-								enim et ducimus beatae. Et velit magni et fugiat
-								voluptas est inventore recusandae eum nihil
-								dolorum sed ipsa totam ea praesentium ipsum qui
-								temporibus saepe.
-							</BaseText>
-						</div>
-					</div>
-				</div>
+				<IntroOverview title="Lorem ipsum dolor sit amet est explicabo blanditiis">
+					<BaseText>
+						Lorem ipsum dolor sit amet. Est explicabo blanditiis eum
+						perferendis harum eum galisum voluptas quo natus nihil
+						aut aspernatur voluptas rem ipsum dolorum aut fugiat
+						cumque. Et officiis iure et asperiores totam quo sunt
+						alias. Sed illum perferendis quo possimus inventore et
+						reiciendis modi. Sed quos voluptate nam deleniti veniam
+						ut nihil consequatur.
+					</BaseText>
+					<BaseText>
+						Et dolorem rerum qui doloremque consectetur aut incidunt
+						fugit ad voluptatibus dignissimos aut laborum excepturi
+						et dicta dicta quo fuga enim. Est ipsum possimus a
+						corporis dolores qui placeat dolor aut veritatis eveniet
+						non cumque autem sed iure veritatis est suscipit sequi.
+						Est asperiores porro non officia incidunt sed autem
+						dolor est dolores illo et molestiae quas aut error
+						totam.
+					</BaseText>
+					<BaseText>
+						Et optio veniam aut veniam voluptatem et explicabo ullam
+						id aperiam cumque aut neque laborum ex voluptatem
+						facere. Eum rerum aperiam eos fugiat dolor et quis
+						cupiditate sed animi enim et ducimus beatae. Et velit
+						magni et fugiat voluptas est inventore recusandae eum
+						nihil dolorum sed ipsa totam ea praesentium ipsum qui
+						temporibus saepe.
+					</BaseText>
+				</IntroOverview>
 
 				<AccommodationList
 					accommodations={accommodations}
