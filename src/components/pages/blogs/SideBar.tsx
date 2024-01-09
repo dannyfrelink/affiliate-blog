0103601df0302878e4f38c441dbca2b52da4629e
dannyfrelink <danny-frelink@hotmail.com>
2024-01-09 14:56:57 +0100

    feat: setup SideBar component

---
+++ src/components/pages/blogs/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SideBarProps {}
+
+const SideBar: React.FC<SideBarProps> = () => {
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+};
+
+export default SideBar;
