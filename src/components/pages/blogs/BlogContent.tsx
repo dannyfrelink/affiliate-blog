c4f0839598f4b974f0e538f6d5b72cc865aae6fa
dannyfrelink <danny-frelink@hotmail.com>
2024-01-10 15:45:54 +0100

    feat: move BlogContent to separate component called BlogContent

---
+++ src/components/pages/blogs/BlogContent.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Container from "../../general/Container";
+import { useAppContext } from "../../../config/AppContext";
+import replaceImageTag from "../../../helpers/replaceImageTag";
+import parseHTMLText from "../../../helpers/parseHTMLText";
+
+interface Images {
+	src: {
+		[image: string]: string;
+	};
+	alt: {
+		[image: string]: string;
+	};
+}
+
+interface BlogContentProps {
+	image?: string;
+	text: string;
+	images: Images;
+}
+
+const BlogContent: React.FC<BlogContentProps> = ({ image, text, images }) => {
+	const { screenSize } = useAppContext();
+	const img = image && require(`../../../images/mockup/${image}`);
+
+	const parsedImg = replaceImageTag(text, images);
+	const parsedText = parseHTMLText(parsedImg, images);
+
+	return (
+		<div className="relative">
+			<Container>
+				<div
+					className={`[&>img]:w-full [&_img]:rounded-2xl [&_img]:shadow-subtle [&>h3]:font-medium ${
+						screenSize < 750
+							? "[&>*:not(:last-child)]:mb-3 [&>h2:not(:first-child)]:mt-6 [&>h2]:!mb-2 [&>h3]:!mb-2 [&>img:not(:last-child)]:mb-4 [&>img]:mt-4"
+							: screenSize < 1250
+							? "[&>*:not(:last-child)]:mb-3.5 [&>h2:not(:first-child)]:mt-7 [&>h2]:!mb-2.5 [&>h3]:!mb-2.5 [&>img:not(:last-child)]:mb-6 [&>img]:mt-6"
+							: "[&>*:not(:last-child)]:mb-4 [&>h2:not(:first-child)]:mt-8 [&>h2]:!mb-3 [&>h3]:!mb-3 [&>img:not(:last-child)]:mb-8 [&>img]:mt-8"
+					}`}
+				>
+					{parsedText}
+				</div>
+			</Container>
+			{img && (
+				<div
+					className={`w-full ${
+						screenSize < 550
+							? "h-[60vh]"
+							: screenSize < 750
+							? "h-[75vh]"
+							: screenSize < 1250
+							? "h-[85vh]"
+							: "h-[85vh]"
+					}`}
+				>
+					<img
+						className={`absolute w-full z-[-99] ${
+							screenSize < 550
+								? "-bottom-32"
+								: screenSize < 750
+								? "-bottom-48"
+								: screenSize < 1250
+								? "-bottom-[16%]"
+								: "-bottom-[28%]"
+						}`}
+						src={img}
+						alt="Backdrop"
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default BlogContent;
--- src/pages/Blogs/BlogPost.tsx
+++ src/pages/Blogs/BlogPost.tsx
@@ -2,16 +2,11 @@ import { useParams } from "react-router-dom";
 import SideBar from "../../components/pages/blogs/SideBar";
 import data from "../../data/blogs.json";
 import { BlogsData, Destination } from "./BlogOverview";
-import Container from "../../components/general/Container";
-import H2 from "../../components/typography/H2";
 import Header from "../../components/header/Header";
-import parseHTMLText from "../../helpers/parseHTMLText";
-import replaceImageTag from "../../helpers/replaceImageTag";
-import { useAppContext } from "../../config/AppContext";
 import Footer from "../../components/general/Footer";
+import BlogContent from "../../components/pages/blogs/BlogContent";
 
 const BlogPost = () => {
-	const { screenSize } = useAppContext();
 	const { id } = useParams();
 	const blogs: BlogsData = data.blogs;
 	const allBlogs: Destination[] = [];
@@ -33,56 +28,15 @@ const BlogPost = () => {
 
 			<div>
 				{sections.map((section, index) => {
-					const image =
-						section.image &&
-						require(`../../images/mockup/${section.image}`);
-					const textTotal = section.text;
-					const parsedImg = replaceImageTag(textTotal, images);
-					const parsedText = parseHTMLText(parsedImg, images);
+					const text = section.text;
 
 					return (
-						<div className="relative" key={index}>
-							<Container>
-								<div
-									className={`[&>img]:w-full [&_img]:rounded-2xl [&_img]:shadow-subtle [&>h3]:font-medium ${
-										screenSize < 750
-											? "[&>*:not(:last-child)]:mb-3 [&>h2:not(:first-child)]:mt-6 [&>h2]:!mb-2 [&>h3]:!mb-2 [&>img:not(:last-child)]:mb-4 [&>img]:mt-4"
-											: screenSize < 1250
-											? "[&>*:not(:last-child)]:mb-3.5 [&>h2:not(:first-child)]:mt-7 [&>h2]:!mb-2.5 [&>h3]:!mb-2.5 [&>img:not(:last-child)]:mb-6 [&>img]:mt-6"
-											: "[&>*:not(:last-child)]:mb-4 [&>h2:not(:first-child)]:mt-8 [&>h2]:!mb-3 [&>h3]:!mb-3 [&>img:not(:last-child)]:mb-8 [&>img]:mt-8"
-									}`}
-								>
-									{parsedText}
-								</div>
-							</Container>
-							{image && (
-								<div
-									className={`w-full ${
-										screenSize < 550
-											? "h-[60vh]"
-											: screenSize < 750
-											? "h-[75vh]"
-											: screenSize < 1250
-											? "h-[85vh]"
-											: "h-[85vh]"
-									}`}
-								>
-									<img
-										className={`absolute w-full z-[-99] ${
-											screenSize < 550
-												? "-bottom-32"
-												: screenSize < 750
-												? "-bottom-48"
-												: screenSize < 1250
-												? "-bottom-[16%]"
-												: "-bottom-[28%]"
-										}`}
-										src={image}
-										alt="Backdrop"
-									/>
-								</div>
-							)}
-						</div>
+						<BlogContent
+							key={index}
+							image={section.image}
+							text={text}
+							images={images}
+						></BlogContent>
 					);
 				})}
 			</div>
