88797b8c67e0e6db41f18a98f984a958241dd079
dannyfrelink <danny-frelink@hotmail.com>
2024-01-11 12:47:11 +0100

    update: add title to NextBlogs section

--- src/components/pages/blogs/SideBar.tsx
+++ src/components/pages/blogs/SideBar.tsx
@@ -3,6 +3,7 @@ import Container from "../../general/Container";
 import { useAppContext } from "../../../config/AppContext";
 import NextBlogs from "./NextBlogs";
 import { Destination } from "../../../pages/Blogs/BlogOverview";
+import H3 from "../../typography/H3";
 
 interface SideBarProps {
 	blogs: Destination[];
@@ -21,7 +22,23 @@ const SideBar: React.FC<SideBarProps> = ({ blogs, id }) => {
 					: `w-[1150px] ${screenSize < 1250 ? "!px-6" : "!px-10"}`
 			}`}
 		>
-			<NextBlogs blogs={blogs} id={id} />
+			<div>
+				<H3
+					className={`text-center ${
+						screenSize < 750
+							? "mb-3"
+							: screenSize < 1000
+							? "mb-4"
+							: `!text-left ${
+									screenSize < 1250 ? "mb-4" : "mb-5"
+							  }`
+					}`}
+				>
+					Ontdek meer
+				</H3>
+
+				<NextBlogs blogs={blogs} id={id} />
+			</div>
 		</Container>
 	);
 };
