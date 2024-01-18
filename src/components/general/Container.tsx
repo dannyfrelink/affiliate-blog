cbabb7ec572ee2a5d7fab41bd866758498142320
dannyfrelink <danny-frelink@hotmail.com>
2024-01-18 16:37:52 +0100

    update: set back previous SideBar component

--- src/components/general/Container.tsx
+++ src/components/general/Container.tsx
@@ -2,21 +2,15 @@ import React from "react";
 import { useAppContext } from "../../config/AppContext";
 
 interface ContainerProps {
-	containerRef?: React.MutableRefObject<null>;
 	children: React.ReactNode;
 	className?: string;
 }
 
-const Container: React.FC<ContainerProps> = ({
-	containerRef,
-	children,
-	className,
-}) => {
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
 	const { screenSize } = useAppContext();
 
 	return (
 		<section
-			ref={containerRef}
 			className={`bg-primary rounded-3xl ${className} ${
 				screenSize < 750
 					? "px-[7.5vw] py-7"
--- src/pages/Blogs/BlogPost.tsx
+++ src/pages/Blogs/BlogPost.tsx
@@ -40,13 +40,7 @@ const BlogPost = () => {
 					className={`z-[1] ${
 						screenSize < 1000
 							? "[&>*:last-child>section]:!rounded-b-none"
-							: `[&>*>section]:rounded-none [&>*:first-child>section]:rounded-tl-2xl ${
-									screenSize < 1250
-										? "w-4/6"
-										: screenSize < 1500
-										? "w-[70%]"
-										: "w-3/4"
-							  }`
+							: "[&>*>section]:rounded-none [&>*:first-child>section]:rounded-tl-2xl"
 					}`}
 				>
 					{sections.map((section, index) => {
