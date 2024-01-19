796f60d8cc939800d9bea2fc36620f25a082653a
dannyfrelink <danny-frelink@hotmail.com>
2024-01-19 14:07:35 +0100

    feat: create component for CountryTag on Blogs pages

--- src/pages/Blogs/BlogOverview.tsx
+++ src/pages/Blogs/BlogOverview.tsx
@@ -3,12 +3,17 @@ import Header from "../../components/header/Header";
 import { useAppContext } from "../../config/AppContext";
 import Overview from "../../components/pages/overview/Overview";
 import BaseText from "../../components/typography/BaseText";
-import IntroOverview from "../../components/pages/overview/IntroOverview";
 import data from "../../data/blogs.json";
 import ListOverview from "../../components/pages/overview/ListOverview";
 import H3 from "../../components/typography/H3";
 import { Link } from "react-router-dom";
 import Footer from "../../components/general/Footer";
+import H2 from "../../components/typography/H2";
+import ThermostatRoundedIcon from "@mui/icons-material/ThermostatRounded";
+import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
+import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
+import FlightRoundedIcon from "@mui/icons-material/FlightRounded";
+import CountryTag from "../../components/pages/blogs/CountryTag";
 
 export interface Destination {
 	id: number;
@@ -41,9 +46,31 @@ export interface BlogsData {
 
 const BlogOverview = () => {
 	const { screenSize } = useAppContext();
-
 	const blogs: BlogsData = data.blogs;
 	const destinations = Object.keys(blogs);
+
+	const tags = [
+		{
+			icon: <ThermostatRoundedIcon />,
+			title: "Beste reistijd",
+			value: "April - Oktober",
+		},
+		{
+			icon: <AttachMoneyRoundedIcon />,
+			title: "Valuta",
+			value: "Indonesische rupiah",
+		},
+		{
+			icon: <AccessTimeFilledRoundedIcon />,
+			title: "Tijdsverschil",
+			value: "6/7 uur",
+		},
+		{
+			icon: <FlightRoundedIcon />,
+			title: "Vliegtijd",
+			value: "16h",
+		},
+	];
 
 	return (
 		<div>
@@ -56,7 +83,53 @@ const BlogOverview = () => {
 			/>
 
 			<Overview destinations={destinations}>
-				<IntroOverview title="Reizen naar Indonesië">
+				<div className="max-w-[1000px] mx-auto">
+					<div
+						className={`${
+							screenSize < 1000
+								? "grid grid-cols-2 gap-x-2 gap-y-6 text-center max-w-[650px] mx-auto mb-7"
+								: `flex ${
+										screenSize < 1250 ? "mb-10" : "mb-14"
+								  }`
+						}`}
+					>
+						{tags.map((tag, index) => {
+							return (
+								<CountryTag
+									title={tag.title}
+									value={tag.value}
+									icon={tag.icon}
+								/>
+							);
+						})}
+					</div>
+
+					<H2 className={screenSize < 1250 ? "mb-2" : "mb-3"}>
+						Reizen naar Indonesië
+					</H2>
+					<div
+						className={
+							screenSize < 1250
+								? "[&>*:not(:last-child)]:mb-3"
+								: "[&>*:not(:last-child)]:mb-4"
+						}
+					>
+						<BaseText>
+							Indonesië is echt onze favoriete reisbestemming! Je
+							kunt hier alles vinden, van prachtige stranden tot
+							groene jungles en van helderblauw water tot een
+							interessante cultuur. Ook kan je ervoor kiezen om
+							goedkoop te reizen, een mooie middenweg te nemen of
+							uit te pakken met mega luxe verblijven. De bevolking
+							is ontzettend gastvrij en behulpzaam en voor de
+							Indische keuken kan je ons echt wakker maken. In de
+							natuur kan je hier van alles vinden, denk aan
+							watervallen, vulkanen, mooie uitzichtpunten en niet
+							te vergeten: rijstvelden!
+						</BaseText>
+					</div>
+				</div>
+				{/* <IntroOverview title="Reizen naar Indonesië">
 					<BaseText>
 						Indonesië is echt onze favoriete reisbestemming! Je kunt
 						hier alles vinden, van prachtige stranden tot groene
@@ -69,7 +142,7 @@ const BlogOverview = () => {
 						denk aan watervallen, vulkanen, mooie uitzichtpunten en
 						niet te vergeten: rijstvelden!
 					</BaseText>
-				</IntroOverview>
+				</IntroOverview> */}
 				<div className="[&>div:first-child>div]:!mt-0">
 					{destinations.map((dest, index) => {
 						const blogsPerDest = blogs[dest];
