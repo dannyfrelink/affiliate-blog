eb1e020a9f0ccebb62e97dda2eca0f70b050d086
dannyfrelink <danny-frelink@hotmail.com>
2024-01-22 14:43:28 +0100

    fix: Accommodaties title running of the screen on mobile

--- src/components/typography/H1.tsx
+++ src/components/typography/H1.tsx
@@ -11,15 +11,16 @@ const H1: React.FC<H1Props> = ({ children, subTitle }) => {
 	const { screenSize } = useAppContext();
 	const location = useLocation();
 	const checkBlog = /\d/.test(location.pathname);
+	const checkAccom = location.pathname === "/accommodaties";
 	const title = children?.toString().toUpperCase();
 
 	return (
 		<h1
 			className={`font-bold max-w-[1000px] ${
 				screenSize < 750
-					? `${checkBlog ? "text-3xl" : "text-5xl"} ${
-							subTitle !== "" && "mb-3"
-					  }`
+					? `${checkAccom && "!text-3xl"} ${
+							checkBlog ? "text-3xl" : "text-5xl"
+					  } ${subTitle !== "" && "mb-3"}`
 					: screenSize < 1250
 					? `${
 							checkBlog
