f5d3cf6a89e64aace89ecd9793689a070eb0f8c2
dannyfrelink <danny-frelink@hotmail.com>
2024-01-23 17:25:05 +0100

    update: add palmtree icon to TableOfContents

--- src/components/pages/blogs/TableOfContents.tsx
+++ src/components/pages/blogs/TableOfContents.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-scroll";
 import H3 from "../../typography/H3";
 import { useAppContext } from "../../../config/AppContext";
 import Details from "../../general/Details";
+import palmTree from "../../../assets/logo/palmtree.svg";
 
 interface TableOfContentsProps {
 	headers: string[];
@@ -15,19 +16,26 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ headers }) => {
 			{screenSize < 1000 ? (
 				<Details summary="Inhoud">
 					<ul
-						className={`px-4 pt-2 pb-6 rounded-2xl [&>a:not(:last-child)]:mb-2 ${
+						className={`px-4 pt-2 pb-6 rounded-2xl [&>a:not(:last-child)]:mb-2.5 ${
 							screenSize < 550 ? "min-w-[250px]" : "min-w-[300px]"
 						}`}
 					>
 						{headers.map((header, index) => (
 							<Link
-								className="cursor-pointer block hover:!text-primary text-sm"
+								className="cursor-pointer hover:!text-primary text-sm flex items-center"
 								to={header.toLowerCase().split(" ").join("-")}
 								smooth={true}
 								offset={-100}
 								duration={500}
 								key={index}
 							>
+								<img
+									className={`mr-3 ${
+										screenSize < 1000 ? "w-2.5" : "w-3"
+									}`}
+									src={palmTree}
+									alt="Palm Tree Icon"
+								/>
 								<li>{header}</li>
 							</Link>
 						))}
@@ -39,16 +47,21 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ headers }) => {
 						Inhoud
 					</H3>
 
-					<ul className="bg-secondary px-5 py-6 text-primary rounded-2xl [&>a:not(:last-child)]:mb-2">
+					<ul className="bg-secondary px-5 py-6 text-primary rounded-2xl [&>a:not(:last-child)]:mb-2.5">
 						{headers.map((header, index) => (
 							<Link
-								className="cursor-pointer block hover:text-primary hover:underline"
+								className="cursor-pointer hover:text-primary hover:underline flex"
 								to={header.toLowerCase().split(" ").join("-")}
 								smooth={true}
 								offset={-100}
 								duration={500}
 								key={index}
 							>
+								<img
+									className="w-3 h-fit mr-3 mt-1"
+									src={palmTree}
+									alt="Palm Tree Icon"
+								/>
 								<li>{header}</li>
 							</Link>
 						))}
