87daf8d4c4de551701c96cccec56245ebf25bdd0
dannyfrelink <danny-frelink@hotmail.com>
2024-01-23 16:44:28 +0100

    fix: hover state for TableOfContents component

--- src/components/pages/blogs/TableOfContents.tsx
+++ src/components/pages/blogs/TableOfContents.tsx
@@ -21,7 +21,7 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ headers }) => {
 					>
 						{headers.map((header, index) => (
 							<Link
-								className="cursor-pointer block hover:underline text-sm"
+								className="cursor-pointer block hover:!text-primary text-sm"
 								to={header.toLowerCase().split(" ").join("-")}
 								smooth={true}
 								offset={-100}
@@ -42,7 +42,7 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ headers }) => {
 					<ul className="bg-secondary px-5 py-6 text-primary rounded-2xl [&>a:not(:last-child)]:mb-2">
 						{headers.map((header, index) => (
 							<Link
-								className="cursor-pointer block hover:underline"
+								className="cursor-pointer block hover:text-primary hover:underline"
 								to={header.toLowerCase().split(" ").join("-")}
 								smooth={true}
 								offset={-100}
