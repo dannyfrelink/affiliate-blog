c07a525a46842b5bf482e1d0533f650e8ef560ba
dannyfrelink <danny-frelink@hotmail.com>
2024-01-24 11:27:03 +0100

    fix: setNavOpen(false) on arrow back

--- src/config/ScrollToTop.tsx
+++ src/config/ScrollToTop.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useAppContext } from "./AppContext";
 
 const ScrollToTop: React.FC = () => {
 	const { pathname } = useLocation();
+	const { setNavOpen } = useAppContext();
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
+		setNavOpen(false);
 	}, [pathname]);
 
 	return null;
