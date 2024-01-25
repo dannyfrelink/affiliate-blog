5ab9108779e35619b8b9ca3ba9a94eae9a513fbe
dannyfrelink <danny-frelink@hotmail.com>
2024-01-25 18:14:00 +0100

    fix: create sticky SideBar that scrolls with the content

--- src/components/pages/blogs/SideBar.tsx
+++ src/components/pages/blogs/SideBar.tsx
@@ -15,10 +15,72 @@ interface SideBarProps {
 const SideBar: React.FC<SideBarProps> = ({ blogs, href }) => {
 	const { screenSize, scrolled, scrolledUp } = useAppContext();
 	const activeBlog = blogs.filter((blog) => blog.href === href)[0];
+	const sidebarRef = useRef<HTMLDivElement>(null);
+	const contentRef = useRef<HTMLDivElement>(null);
+	const [oldScroll, setOldScroll] = useState<number>(0);
+	const [topValue, setTopValue] = useState<number>(0);
+	const [initialStick, setInitialStick] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (contentRef.current && sidebarRef.current) {
+			let viewportHeight = window.innerHeight;
+			let contentHeight =
+				contentRef.current.getBoundingClientRect().height;
+			let sidebarTop =
+				sidebarRef.current.getBoundingClientRect().top + window.scrollY;
+
+			if (scrolled >= sidebarTop) {
+				if (scrolledUp) {
+					// When the user scrolls back up
+					let scrollAmount = oldScroll - scrolled;
+
+					if (topValue > -100) {
+						setTopValue((prevValue) => prevValue - scrollAmount);
+					} else {
+						setTopValue(-100);
+					}
+
+					setOldScroll(scrolled);
+				} else if (
+					initialStick &&
+					topValue < contentHeight - viewportHeight + 50
+				) {
+					// When the user scrolls back down midway blog
+					if (topValue === -100) {
+						setTopValue(-40);
+					} else {
+						let scrollAmount = oldScroll - scrolled;
+
+						setTopValue((prevValue) => prevValue - scrollAmount);
+					}
+
+					setOldScroll(scrolled);
+				} else {
+					// When the user reaches the bottom of the content
+					setTopValue(contentHeight - viewportHeight + 50);
+					setInitialStick(true);
+					setOldScroll(scrolled);
+				}
+
+				// Set styling of position and top
+				contentRef.current.style.top = initialStick
+					? `${-topValue}px`
+					: "";
+				contentRef.current.style.position = topValue ? "sticky" : "";
+			} else {
+				setInitialStick(false);
+
+				// Remove styling of position and top
+				contentRef.current.style.position = "";
+				contentRef.current.style.top = "";
+			}
+		}
+	}, [scrolled]);
 
 	return (
 		<Container
-			className={`rounded-none rounded-tr-2xl ${
+			containerRef={sidebarRef}
+			className={`relative rounded-none rounded-tr-2xl ${
 				screenSize < 1250
 					? "w-2/6 !px-10"
 					: `!px-12 ${
@@ -26,7 +88,7 @@ const SideBar: React.FC<SideBarProps> = ({ blogs, href }) => {
 					  }`
 			}`}
 		>
-			<div className={`sticky top-0 [&>*:not(:last-child)]:mb-10`}>
+			<div ref={contentRef} className={`[&>*:not(:last-child)]:mb-10`}>
 				<TableOfContents headers={activeBlog.headers} />
 
 				<section>
