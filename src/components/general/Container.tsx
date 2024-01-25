cde78f4bc88f441dfa114026a35ade2ded5d01a3
dannyfrelink <danny-frelink@hotmail.com>
2024-01-25 16:06:20 +0100

    update: trying to make SideBar scroll and remain sticky (not working)

--- src/components/general/Container.tsx
+++ src/components/general/Container.tsx
@@ -4,13 +4,23 @@ import { useAppContext } from "../../config/AppContext";
 interface ContainerProps {
 	children: React.ReactNode;
 	className?: string;
+	containerRef?: React.RefObject<HTMLDivElement>;
+	style?: {
+		top: number;
+	};
 }
 
-const Container: React.FC<ContainerProps> = ({ children, className }) => {
+const Container: React.FC<ContainerProps> = ({
+	children,
+	className,
+	containerRef,
+	style,
+}) => {
 	const { screenSize } = useAppContext();
 
 	return (
 		<section
+			ref={containerRef}
 			className={`bg-primary rounded-3xl ${className} ${
 				screenSize < 750
 					? "px-[7.5vw] py-7"
@@ -18,6 +28,7 @@ const Container: React.FC<ContainerProps> = ({ children, className }) => {
 					? "px-[9vw] py-12"
 					: "px-[10vw] py-16"
 			}`}
+			style={style}
 		>
 			{children}
 		</section>
--- src/components/pages/blogs/SideBar.tsx
+++ src/components/pages/blogs/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Container from "../../general/Container";
 import { useAppContext } from "../../../config/AppContext";
 import NextBlogs from "./NextBlogs";
@@ -15,20 +15,66 @@ interface SideBarProps {
 const SideBar: React.FC<SideBarProps> = ({ blogs, href }) => {
 	const { screenSize, scrolled, scrolledUp } = useAppContext();
 	const activeBlog = blogs.filter((blog) => blog.href === href)[0];
+	const sidebarRef = useRef<HTMLDivElement>(null);
+	// const [oldRefScroll, setOldRefScroll] = useState<number>(0);
+	const [oldScroll, setOldScroll] = useState<number>(0);
+	const [stickyTop, setStickyTop] = useState<number>(0);
+	const stickyTopRef = useRef<number>(0);
+
+	useEffect(() => {
+		if (
+			sidebarRef.current &&
+			sidebarRef.current.getBoundingClientRect().y < 0
+		) {
+			const refScroll = sidebarRef.current.getBoundingClientRect().y;
+
+			if (scrolledUp) {
+				const scrollAmount = oldScroll - scrolled;
+
+				console.log(stickyTopRef.current);
+
+				if (stickyTopRef.current < 50) {
+					window.requestAnimationFrame(() => {
+						stickyTopRef.current += scrollAmount;
+					});
+				} else {
+					stickyTopRef.current = 50;
+				}
+				setOldScroll(scrolled);
+			} else if (refScroll > -1050) {
+				stickyTopRef.current = Math.round(refScroll);
+				setOldScroll(scrolled);
+			} else {
+				const scrollAmount = oldScroll - scrolled;
+
+				if (stickyTopRef.current > -1050 && scrollAmount > 0) {
+					window.requestAnimationFrame(() => {
+						stickyTopRef.current -= scrollAmount;
+					});
+				} else {
+					stickyTopRef.current = -1050;
+				}
+
+				setOldScroll(scrolled);
+			}
+		}
+	}, [scrolled]);
 
 	return (
 		<Container
-			className={`sticky h-full rounded-none rounded-tr-2xl ${
+			// containerRef={sidebarRef}
+			className={`relative min-h-full rounded-none rounded-tr-2xl ${
 				screenSize < 1250
-					? "w-2/6"
-					: screenSize < 1500
-					? "w-[30%]"
-					: "w-[27.5%]"
-			} ${scrolled > 600 && scrolledUp ? "top-[2.5rem]" : "-top-8"} ${
-				screenSize < 1250 ? "!px-10" : "!px-12"
-			} ${screenSize > 1500 && "!px-16"}`}
+					? "w-2/6 !px-10"
+					: `!px-12 ${
+							screenSize < 1500 ? "w-[30%]" : "w-[27.5%] !px-16"
+					  }`
+			}`}
 		>
-			<div className="[&>*:not(:last-child)]:mb-10">
+			<div
+				ref={sidebarRef}
+				className={`sticky top-[${stickyTopRef.current}px] h-fit [&>*:not(:last-child)]:mb-10`}
+			>
 				<TableOfContents headers={activeBlog.headers} />
 
 				<section>
